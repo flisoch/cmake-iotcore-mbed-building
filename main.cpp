8e4bf95f76f820ad6792d1d8316fb9ea71516842
flisoch <flisochcs@gmail.com>
2021-09-28 02:23:15 +0300

    mbed&main building is ok. adding iotcore breaks everything. iotcore has all include folders of mbedos

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2021 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <cstdio>
+#include "mbed.h"
+
+
+int main()
+{
+    printf("Hello, Mbed!\n");
+    return 0;
+}
